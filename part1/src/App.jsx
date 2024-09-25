d4d162b90ef98d25ec1c20a3a77eeb2c34a52390
rinne13 <varvararinne@gmail.com>
2024-09-25 12:25:22 +0300

    all tasks are done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,71 +1,83 @@
 import { useState } from 'react'
-import Button from './Button'
 
 const App = () => {
-  
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-      ]
-
-      const [selected, setSelected] = useState(0)
-
-      const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [showTable, setShowTable] = useState(false)
 
   const handleNextClick = () => {
-
-    const randomIndex =
-    Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomIndex);
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
   const handleVoteClick = () => {
-    const votesCopy = [...votes];
-    votesCopy[selected] += 1;
-    setVotes(votesCopy);
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
   }
 
+  const mostVotesIndex = votes.indexOf(Math.max(...votes))
 
-   
+  const toggleTableVisibility = () => {
+    setShowTable(!showTable)
+  }
 
   return (
     <div>
       <h1>Current Anecdote</h1>
-
       <div>
         <div>{anecdotes[selected]}</div>
         <div>has {votes[selected]} votes</div>
-        <button onClick={handleVoteClick}>Vote</button>
+        <button onClick={handleVoteClick}  style={{ marginTop: '2rem' }}>Vote</button>
         <button onClick={handleNextClick}>Next anecdote</button>
       </div>
-      
-      <h2>Programming Anecdotes</h2>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Anecdote</th>
-            <th>Votes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {anecdotes.map((anecdote, index) => (
-            <tr key={index}>
-              <td>{anecdote}</td>
-              <td>{votes[index]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      
+      <div>
+        <h2>Anecdote with most votes</h2>
+        <div>{anecdotes[mostVotesIndex]}</div>
+        <div>has {votes[mostVotesIndex]} votes</div>
+      </div>
+
+<div>
+    
+      <button onClick={toggleTableVisibility}  style={{ marginTop: '2rem' }}>
+        {showTable ? 'Hide Result of Anecdotes' : 'Show Result of Anecdotes'}
+      </button>
+
+     
+      {showTable && (
+        <div>
+          <h3>Programming Anecdotes</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Anecdote</th>
+                <th>Votes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {anecdotes.map((anecdote, index) => (
+                <tr key={index}>
+                  <td>{anecdote}</td>
+                  <td>{votes[index]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      </div>
     </div>
   )
 }
