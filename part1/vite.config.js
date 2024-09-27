c97a704ecb75e5f65070a01ab0480a009f8bbcc3
rinne13 <varvararinne@gmail.com>
2024-09-27 10:34:55 +0300

    added Deploying Vite / React App to GitHub Pages

--- part1/vite.config.js
+++ part1/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-perusteet-02-soveltavat-tehtavat-03-anekdootit",
+
   plugins: [react()],
 })
